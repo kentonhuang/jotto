a3189fac2ed23209c34addf0b4b65610097743f1
Kenton Huang <huangkenton@gmail.com>
2018-07-31 15:08:50 -0700

    add proptypes for congrats component

--- src/Congrats.js
+++ src/Congrats.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 /**
  * Function react component for congratulatory message.
@@ -22,5 +23,9 @@ const Congrats = (props) => {
 			);
 		}
 };
+
+Congrats.propTypes = {
+	success: PropTypes.bool.isRequired,
+}
 
 export default Congrats;

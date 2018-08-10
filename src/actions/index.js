38deec7132114d1b174a8dc2241e4940d23fa150
Kenton Huang <huangkenton@gmail.com>
2018-08-09 17:26:55 -0700

    Set up guessWords test

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,26 @@
+import { SSL_OP_ALLOW_UNSAFE_LEGACY_RENEGOTIATION } from "constants";
+
 export const actionTypes = {
 	CORRECT_GUESS: 'CORRECT_GUESS',
+	GUESS_WORD: 'GUESSED_WORD',
 };
 
+// /**
+//  * @function correctGuess
+//  * @returns {object} - Action object with type `CORRECT_GUESS`
+//  */
+// export function correctGuess() {
+// 	return { type: actionTypes.CORRECT_GUESS };
+// }
+
 /**
- * @function correctGuess
- * @returns {object} - Action object with type `CORRECT_GUESS`
+ * Returns Redux Thunkf unction that dispatches GUESS_WORD action and (conditionally) CORRECT_GUESS action
+ * @function guessWord
+ * @param {string} guessedWord - Guessed word.
+ * @returns {function} - Redux Thunk function
  */
-export function correctGuess() {
-	return { type: actionTypes.CORRECT_GUESS };
-}
+export const guessWord = (guessedWord) => {
+	return function(dispatch, getState) {
+
+	};
+};
